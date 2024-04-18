2b133419fdca4df8b7ff449fa658b562a21f7c6a
cjs <123@abc.com>
2024-04-18 09:12:08 +0800

    kkb 2 父子组件互相传递数据，demo

---
+++ vue_test_2_full_2018_5_5/src/components/list.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div>
+    <span style="font-size:30px;"></span>
+    已选中：{{count}}件
+    <ul>
+<!--      <ListItem str="qewd"/>-->
+<!--      <ListItem str="vdes"/>-->
+<!--      <ListItem str="rvhn"/>-->
+<!--      <ListItem str="egcb"/>-->
+<!--      <ListItem str="ykmy"/>-->
+<!--      <ListItem str="5ybk"/>-->
+      <ListItem v-for="item in items" :key="item.name" :item="item" :plus="plusOne" :minus="minusOne"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from '@/components/list_item'
+
+export default {
+  data () {
+    return {items: [
+      {name: '1', price: '2', sales: '3'},
+      {name: '4', price: '5', sales: '6'},
+      {name: '7', price: '8', sales: '9'},
+      {name: '10', price: '11', sales: '12'}
+    ],
+    count: 0
+    }
+  },
+  components: {ListItem},
+  methods: {
+    plusOne () {
+      this.count++
+    },
+    minusOne () {
+      this.count--
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
---
+++ vue_test_2_full_2018_5_5/src/components/list_item.vue
@@ -0,0 +1,41 @@
+<template lang="html">
+  <div>
+    <li>
+      <input type="checkbox" value="请选择" name="" v-model="checkeding">{{checkeding}}
+      {{ data.name }}--{{ data.price }}--{{ data.sales }}
+    </li>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      checkeding: false,
+      data: ''
+    }
+  },
+  mounted () { // 钩子——事件
+    // 钩子能组织事件
+    console.log(this.$attrs)
+    // this.txt = this.$attrs['str']
+    this.data = this.$attrs.item
+  },
+  watch: {
+    // 函数名称要和vue实例的属性名一致
+    checkeding () {
+      if (this.checkeding) {
+        console.log('123wef')
+        this.$attrs.plus()
+      } else {
+        console.log('123wefwefwe')
+        this.$attrs.minus()
+      }
+    }
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
