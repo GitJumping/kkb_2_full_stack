b2f04296cb1c3915dd7957a5190c1165df802335
cjs <123@abc.com>
2024-04-18 14:53:11 +0800

    规范每节课程的代码，vue_test_2_full_2018_5_5父子组件通信，axios请求代码

---
+++ vue_test_2_full_2018_5_5_server/server.js
@@ -0,0 +1,24 @@
+const koa = require('koa')
+const router = require('koa-router')
+
+// eslint-disable-next-line new-cap
+let server = new koa()
+server.listen(8081)
+
+server.use(async (ctx, next) => {
+  ctx.set('Access-Control-Allow-Origin', '*')
+  await next()
+})
+
+// eslint-disable-next-line new-cap
+let r1 = new router()
+r1.get(`/list`, async ctx => {
+  ctx.body = [
+    {name: '手表', price: '8945.9', sales: 2342},
+    {name: '玩具', price: '234.78', sales: 4568},
+    {name: '衣服', price: '238.46', sales: 3458},
+    {name: '毛巾', price: '89345.45', sales: 123890}
+  ]
+})
+
+server.use(r1.routes())
